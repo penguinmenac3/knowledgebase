e48ff38872f672c3d52eb922eb273ed579e7de9e
Michael Fuerst <mail@michaelfuerst.de>
2023-04-13 13:33:19 +0200

    Add form to the framework.

---
+++ src/framework/form.ts
@@ -0,0 +1,55 @@
+import { Button } from "./button";
+import { Module } from "./module";
+
+export class Form extends Module<HTMLDivElement> {
+    constructor(...modules: Module<HTMLElement>[]) {
+        super("div")
+        for (const module of modules) {
+            this.add(module)
+        }
+    }
+
+    public submit() {
+        let params = new FormData()
+        for (const key in this.htmlElement.children) {
+            let module = this.htmlElement.children[key]
+            if (module instanceof HTMLInputElement) {
+                params.append(module.name, module.value)
+            }
+        }
+        this.onSubmit(params)
+    }
+
+    public onSubmit(formData: FormData) {
+        console.log("Form::onSubmit: Not implemented! Must be implemented by subclass.")
+    }
+}
+
+export class FormInput extends Module<HTMLInputElement> {
+    constructor(name: string, placeholder: string, type: string, cssClass: string = "") {
+        super("input")
+        this.htmlElement.name = name
+        this.htmlElement.placeholder = placeholder
+        this.htmlElement.type = type
+        if (cssClass != "") {
+            this.setClass(cssClass)
+        }
+    }
+}
+
+export class FormLabel extends Module<HTMLLabelElement> {
+    constructor(text: string, cssClass: string = "") {
+        super("label")
+        this.htmlElement.innerText = text
+        if (cssClass != "") {
+            this.setClass(cssClass)
+        }
+    }
+}
+
+export class FormSubmit extends Button {
+    public onClick() {
+        let parent = this.parent as Form
+        parent.submit()
+    }
+}
